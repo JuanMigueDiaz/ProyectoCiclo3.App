d9eda855f2bb0c1e5eaca5b2e1bffdcf1fcb0cf8
JuanMigueDiaz <diazgutierrezjuanmiguel@gamil.com>
2022-08-28 23:14:52 -0500

    se agrega el proyecto

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Buses{
+
+        public int id {get;set}
+        public int numero_asientos {get;set;}
+        public string Rutas {get;set;}
+        public int modelo {get;set;}  
+        public string placa {get;set;} 
+    }    
+}
+
